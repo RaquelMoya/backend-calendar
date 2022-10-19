249b2b58022f952f6dacc2b5c8b4f4c5530cbfc7
unknown <rakelms90@gmail.com>
2022-10-19 12:17:10 +0200

    changes in events

--- controllers/events.js
+++ controllers/events.js
@@ -1,17 +1,10 @@
 const { response } = require('express');
 const Evento = require('../models/Evento');
 
+const getEventos = async( req, res = response ) => {
 
-// {
-//     ok: true,
-//     msg:'mensaje'
-// }
-
-
-const getEventos = async(req, res = response) => {
-    
     const eventos = await Evento.find()
-                                .populate('user', 'name');
+                                .populate('user','name');
 
     res.json({
         ok: true,
@@ -19,23 +12,24 @@ const getEventos = async(req, res = response) => {
     });
 }
 
-const crearEvento = async(req, res = response) => {
-        
-    const evento = new Evento(req.body);
+const crearEvento = async ( req, res = response ) => {
+
+    const evento = new Evento( req.body );
 
     try {
 
         evento.user = req.uid;
-
+        
         const eventoGuardado = await evento.save();
 
         res.json({
             ok: true,
             evento: eventoGuardado
-        });
+        })
+
 
     } catch (error) {
-        console.log(error);
+        console.log(error)
         res.status(500).json({
             ok: false,
             msg: 'Hable con el administrador'
@@ -43,23 +37,23 @@ const crearEvento = async(req, res = response) => {
     }
 }
 
-const actualizarEvento = async(req, res = response) => {
-                
+const actualizarEvento = async( req, res = response ) => {
+    
     const eventoId = req.params.id;
     const uid = req.uid;
 
     try {
 
-        const evento = await Evento.findById(eventoId);
+        const evento = await Evento.findById( eventoId );
 
-        if(!evento){
+        if ( !evento ) {
             return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
         }
 
-        if(evento.user.toString() !== uid){
+        if ( evento.user.toString() !== uid ) {
             return res.status(401).json({
                 ok: false,
                 msg: 'No tiene privilegio de editar este evento'
@@ -71,13 +65,14 @@ const actualizarEvento = async(req, res = response) => {
             user: uid
         }
 
-        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {new: true});
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
 
         res.json({
             ok: true,
             evento: eventoActualizado
         });
 
+        
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -85,38 +80,38 @@ const actualizarEvento = async(req, res = response) => {
             msg: 'Hable con el administrador'
         });
     }
+
 }
 
-const eliminarEvento = async(req, res = response) => {
-                        
+const eliminarEvento = async( req, res = response ) => {
+
     const eventoId = req.params.id;
     const uid = req.uid;
 
     try {
 
-        const evento = await Evento.findById(eventoId);
+        const evento = await Evento.findById( eventoId );
 
-        if(!evento){
+        if ( !evento ) {
             return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
         }
 
-        if(evento.user.toString() !== uid){
+        if ( evento.user.toString() !== uid ) {
             return res.status(401).json({
                 ok: false,
                 msg: 'No tiene privilegio de eliminar este evento'
             });
         }
 
-        await Evento.findByIdAndDelete(eventoId);
 
-        res.json({
-            ok: true,
-            msg: 'Evento eliminado'
-        });
+        await Evento.findByIdAndDelete( eventoId );
 
+        res.json({ ok: true });
+
+        
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -124,7 +119,9 @@ const eliminarEvento = async(req, res = response) => {
             msg: 'Hable con el administrador'
         });
     }
+
 }
+
 
 module.exports = {
     getEventos,
